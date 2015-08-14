26095c35090ce82cdffa081886cd9cd1ea2674a4
metachetnasharma <chetna.sharma@metacube.com>
2015-08-14 18:23:41 +0530

    completed

---
+++ OOP/OOP_Assignment1/Car_Service/Car.java
@@ -0,0 +1,14 @@
+package Car_Service;
+
+	public class Car
+	{
+		String carNumber;
+		String carType;
+
+		public Car(String CarNum, String CarTyp)
+		{
+			carNumber = CarNum;
+			carType = CarTyp;
+		}
+	}
+
---
+++ OOP/OOP_Assignment1/Car_Service/People.java
@@ -0,0 +1,11 @@
+package Car_Service;
+
+public abstract class People
+{
+	String mechID;
+	String name;
+	// OTHER COMMON DETAILS FOR PEOPLE
+}
+
+
+
