72481a4e2fc8a9a03b78d9231d2b297b6d77143d
metachetnasharma <chetna.sharma@metacube.com>
2015-08-24 18:32:50 +0530

    completed

---
+++ OOP/CASESTUDYPHASE1/VehiclePrinter.java
@@ -0,0 +1,14 @@
+package session1;
+
+/**
+ * @author Chetna--- Class to print the object details
+ * 
+ */
+public class VehiclePrinter {
+	public static String printVehicleSpecification(Vehicle vehicle) {
+
+		return vehicle.toString();// returning the object values
+
+	}
+
+}
