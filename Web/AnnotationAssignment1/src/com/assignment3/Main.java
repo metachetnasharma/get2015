911d1949e88c423ceee469573e6caa3d2d7947c2
metachetnasharma <chetna.sharma@metacube.com>
2015-10-16 18:26:55 +0530

    completed

---
+++ Web/AnnotationAssignment1/src/com/assignment3/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.assignment3;
+
+/**
+ * @author chetna
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AnnotationRunner annotationRunner=new AnnotationRunner();
+		annotationRunner.method1();
+		annotationRunner.method2();
+		annotationRunner.method3();
+		annotationRunner.method4();
+		annotationRunner.method5();
+
+	}
+
+}
