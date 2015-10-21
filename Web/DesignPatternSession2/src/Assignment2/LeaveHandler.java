dda8228c15bbb3f89033111a859345fce43a467e
metachetnasharma <chetna.sharma@metacube.com>
2015-10-21 18:35:17 +0530

    completed

---
+++ Web/DesignPatternSession2/src/Assignment2/LeaveHandler.java
@@ -0,0 +1,38 @@
+package Assignment2;
+
+/**
+ * @author Chetna
+ *
+ */
+public abstract class LeaveHandler {
+
+	public final static int MENTOR = 1;
+	public final static int SENIOR_MENTOR = 2;
+	public final static int HR_MANAGER = 5;
+
+	protected int day;
+
+	protected LeaveHandler successor;
+
+	public void setSucessor(LeaveHandler successor) {
+		this.successor = successor;
+	}
+
+	public void handleRequest(int day) {
+
+		if (this.day >= day) {
+			leaveApprovalMessage();
+		}
+		else
+		if (this.successor != null) {
+			this.successor.handleRequest(day);
+		}
+		else {
+			System.out.println("Leave can't be approved");
+		}
+
+	}
+
+	abstract protected void leaveApprovalMessage();
+
+}
